47e142aec51f2e1aa7d97f5b3bbf953e33f401c2
saju <saju.webmaker@gmail.com>
2024-08-24 03:17:21 +0600

    nwe

--- resources/views/emails/welcome.blade.php
+++ resources/views/emails/welcome.blade.php
@@ -12,26 +12,16 @@
     <div style="margin: auto;">
         <div style="width: 500px; text-align: center; border: black 1px solid;">
             <div style="background-color: #EDEADE;padding: 5px 5px;">
-                <a href="https://afreel.com/"><img style="max-width: 300px;" src="https://afreel.com/user/img/logo.png" alt=""></a>
+                <a href="https://djassa.net/"><img style="max-width: 300px;" src="https: //ibb.co/yfVLwHq
+" alt=""></a>
                 <p>Connecter l’Afrique pour libérer le talent</p>
             </div>
             <h1>Hi <span>{{ $get_user_name}}</span></h1>
-            <h2>-Merci d’être avec nous!</h2>
-            <h2>Vérifier votre email</h2>
-            <div>
+           <div>
                 <span style="background: red; color: white; padding: 15px 10px; font-size: 30px; border-radius: 5px;">{{
                     $validToken}}</span>
             </div>
-            <div style="margin-top: 20px;">
-                <a href="https://afreel.com/">aller sur le site</a>
-            </div>
-            <div style="background-color: #172228; padding: 5px 5px; color: white;">
-                <p style="padding: 10px;">À propos de la société</p>
-                <p style="padding: 10px;">XCompany Côte d’Ivoire, entreprise de création de solutions web et de mise en
-                    place de
-                    systèmes démotiques et
-                    électroniques de maisons connectés et d’installations de dispositifs électroniques</p>
-            </div>
+
         </div>
     </div>
 </body>
