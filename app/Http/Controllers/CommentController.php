13d29f1fdc43f5fb4c4abecfbd9ede44d5676f46
saju <saju.webmaker@gmail.com>
2024-09-16 03:29:02 +0600

    review

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Comment;
+use App\Models\Order;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -34,16 +36,38 @@ class CommentController extends Controller
             'rating' => 'required|string|max:255',
             'comment' => 'required|string|max:255',
         ]);
-        $user = Auth::user();
 
-        $comment = new comment();
-        // dd($request->post_id);
+        $order = Order::where('add_id', $request->post_id)
+            ->orderBy('created_at', 'desc')
+            ->first();
 
-        $comment->user_id = $user->id;
-        $comment->post_id = $request->post_id;
-        $comment->rating = $request->rating;
-        $comment->comment = $request->comment;
-        $comment->save();
+        $daysPassed = Carbon::now()->diffInDays($order->updated_at);
+
+        // dd($order->status);
+        if (empty($order->status)) {
+            toastr()->error('', 'You can comment after the buy!');
+        } else {
+            if ($order->status == "Delivered") {
+                if ($daysPassed < 3) {
+                    $user = Auth::user();
+                    $comment = new comment();
+                    $comment->user_id = $user->id;
+                    $comment->post_id = $request->post_id;
+                    $comment->rating = $request->rating;
+                    $comment->comment = $request->comment;
+                    $comment->save();
+
+                } else {
+                    toastr()->error('', 'You can comment within 3 days!');
+
+                }
+
+            } else {
+                toastr()->error('', 'You can comment after the delivery!');
+            }
+
+        }
+
         return redirect()->back();
 
     }
